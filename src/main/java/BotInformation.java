cf86539ee8bb0ca9c720e44e5e5732381288ee35
Tony Abou-Zeidan <56143916+tony-zeidan@users.noreply.github.com>
2020-10-01 13:27:00 -0400

    Delete BotInformation.java

--- src/main/java/BotInformation.java
+++
@@ -1,4 +0,0 @@
-public class BotInformation {
-    public static final String BOT_TOKEN = "NTU0NzA0OTY2MTU0MDU5ODA4.XjhKPg.SQ6e8V5LQ75LWHEcposzdKEJTDs";
-    public static final String BOT_PREFIX = "r/";
-}
