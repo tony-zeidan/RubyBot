9283e03349c22033e7ea337b41cf927ed9f7292b
Tony Zeidan <tony.azp25@gmail.com>
2020-10-13 12:41:03 -0400

    All project structure has been changed.

---
+++ src/main/java/ruby/core/BotInformation.java
@@ -0,0 +1,6 @@
+package ruby.core;
+
+public class BotInformation {
+    public static final String BOT_TOKEN = "NTU0NzA0OTY2MTU0MDU5ODA4.XIaPQQ.118l-ZKvas_fAqoEAPUkqJeUjQY";
+    public static final String BOT_PREFIX = "r/";
+}
