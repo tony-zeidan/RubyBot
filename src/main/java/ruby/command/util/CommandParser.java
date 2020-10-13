f388724f0798887442939d5021122945816b486f
Tony Zeidan <tony.azp25@gmail.com>
2020-10-13 13:03:36 -0400

    CommandParser modified.

--- src/main/java/ruby/command/util/CommandParser.java
+++ src/main/java/ruby/command/util/CommandParser.java
@@ -5,8 +5,18 @@ import net.dv8tion.jda.api.entities.Message;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * CommandParser class provides functions to aid in parsing the information found in a valid command,
+ * when it is input into discord.
+ */
 public class CommandParser {
 
+    /**
+     * Retrieve the arguments found within a message representing a valid command.
+     *
+     * @param msg The message that contains the valid message
+     * @return The list containing the arguments found within the message
+     */
     public static List<String> getArguments(Message msg) {
 
         //split the message contents
